2c8d48347f8c7a576b5e121aab85049337e47d9f
ddijk <dick@dijkrosoft.nl>
2021-12-22 14:58:44 +0100

    Add semantic-ui component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TableExampleSortable from './TableExampleSortable';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TableExampleSortable />
   </React.StrictMode>,
   document.getElementById('root')
 );
